7de5e5d3a711ea5358664176208c6399ef0acbaa
rajaspar <parthiban.rajasekaran@diconium.com>
2022-07-29 16:41:11 +0200

    clean up code - dry principle

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /* console.log(document.querySelector('.message').textContent);
 
@@ -10,47 +10,56 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
  */
+const setBackgroundImage = function (imageUrl) {
+  document.querySelector("body").style.background = imageUrl;
+};
+
+const setScore = function (score) {
+  document.querySelector(".score").textContent = score;
+};
+
+const setMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let numberOfTriesAttempted = 10;
 let highscore = 0;
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   numberOfTriesAttempted = 10;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = numberOfTriesAttempted;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.background = "url('default.jpg')";
+  setMessage("Start guessing...");
+  setScore(numberOfTriesAttempted);
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  setBackgroundImage("url('default.jpg')");
 });
 
-document.querySelector('.check').addEventListener('click', function () {
-  const enteredNumber = Number(document.querySelector('.guess').value);
-  console.log(typeof enteredNumber);
+document.querySelector(".check").addEventListener("click", function () {
+  const enteredNumber = Number(document.querySelector(".guess").value);
 
   if (!enteredNumber) {
-    document.querySelector('.message').textContent = '⛔️ Not a valid input';
+    setMessage("⛔️ Not a valid input");
   } else if (enteredNumber === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct number';
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.background = "url('win.jpg')";
-    document.querySelector('.number').style.width = '30rem';
+    setMessage("🎉 Correct number");
+    document.querySelector(".number").textContent = secretNumber;
+    setBackgroundImage("url('win.jpg')");
+    document.querySelector(".number").style.width = "30rem";
 
     if (highscore < numberOfTriesAttempted) {
       highscore = numberOfTriesAttempted;
-      document.querySelector('.highscore').textContent = highscore;
+      document.querySelector(".highscore").textContent = highscore;
     }
   } else if (enteredNumber !== secretNumber) {
     if (numberOfTriesAttempted > 1) {
-      document.querySelector('.message').textContent =
-        numberOfTriesAttempted > secretNumber ? '⬆️ Too high' : '⬇️ Too Low';
+      setMessage(enteredNumber > secretNumber ? "⬆️ Too high" : "⬇️ Too Low");
       numberOfTriesAttempted--;
-      document.querySelector('.score').textContent = numberOfTriesAttempted;
+      setScore(numberOfTriesAttempted);
     } else {
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.background = "url('lose.jpg')";
-      document.querySelector('.message').textContent = '💔 You lost the game';
+      setScore(0);
+      setBackgroundImage("url('lose.jpg')");
+      setMessage("💔 You lost the game");
     }
   }
 });
